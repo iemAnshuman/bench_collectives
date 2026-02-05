4e6bf08be65997bd0d508067ac1e18e2770fa5e7
constracktor <74077030+constracktor@users.noreply.github.com>
2026-02-05 07:02:37 -0600

    Update mpi benchmark structure

--- compile.sh
+++ compile.sh
@@ -1,16 +1,16 @@
 set -e
+ROOT=$(pwd)
 #spack load /ezc6ffn #openmpi
-module load openmpi/5.0.5
-spack load cxxopts
-mpicxx mpi_benchmark.cpp -I $(spack location -i cxxopts)/include -o test_mpi
+module load gcc/14.2.0
+spack load openmpi@5.0.5%gcc@14.2.0 arch=linux-almalinux9-skylake_avx512
+#module load openmpi/5.0.5
+#spack load cxxopts
+BUILD_DIR_MPI=${ROOT}/build/mpi/bin
+mkdir -p ${BUILD_DIR_MPI}
+cd ${BUILD_DIR_MPI}
+mpicxx ${ROOT}/mpi/mpi_benchmark.cpp -I $(spack location -i cxxopts)/include -o benchmark_collectives_mpi
 
+# TODO add hpx
 #spack load gcc@14.2.0
 module load gcc/14.2.0
-spack env activate dev_env/.
-spack install
-rm -rf build
-mkdir build
-cd build
-cmake .. -DHPX_IGNORE_BOOST_COMPATIBILITY=ON
-make
-mv test_hpx ../test_hpx
+#spack env activate dev_env/.
