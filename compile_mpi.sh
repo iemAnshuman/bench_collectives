2ba485a74fb82fc6452395a76325d1c2b1f460f5
constracktor <74077030+constracktor@users.noreply.github.com>
2026-03-29 06:03:25 -0500

    Adjust MPI benchmark

--- compile_mpi.sh
+++ compile_mpi.sh
@@ -2,11 +2,11 @@ set -e
 # Load dependencies
 module load gcc/14.2.0
 module load openmpi/5.0.5
-spack install cxxopts
+spack install cxxopts%gcc@14.2.0
 
 # Compile
 ROOT=$(pwd)
 BUILD_DIR_MPI=${ROOT}/build/mpi/bin
 mkdir -p ${BUILD_DIR_MPI}
 cd ${BUILD_DIR_MPI}
-mpicxx ${ROOT}/mpi/mpi_benchmark.cpp -I $(spack location -i cxxopts)/include -o benchmark_collectives_test
+mpicxx ${ROOT}/mpi/mpi_benchmark.cpp -I $(spack location -i cxxopts)/include -o benchmark_collectives_mpi
